ffcb718a0d10e0d9471475edaea9bb26380b6541
Nahufacundo <nahufacundo1698@gmail.com>
2023-04-02 20:38:04 -0300

    Primeros Archivos

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+export const Login = () => {
+
+    let nombre = "Facundo"
+
+    const saludar = () => {
+        console.log("buenas tardes")
+    }
+    return <div style={{background: "steelblue"}}>
+        <h1>Hola desde el componente login</h1>
+        <h3> Mi nombre es {nombre} </h3>
+        <button onClick={saludar}>Saludar</button>
+    </div>
+}
