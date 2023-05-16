c9fd7f36135b4a4770ee7be7b01ac91c5b028c6c
Nahufacundo <nahufacundo1698@gmail.com>
2023-05-16 13:04:28 -0300

    se agregan variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import {getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArWDHUbWoQECfL_Min5Z81ACYsKTOsI40",
-  authDomain: "ecommerce-reactjs-4c7fe.firebaseapp.com",
-  projectId: "ecommerce-reactjs-4c7fe",
-  storageBucket: "ecommerce-reactjs-4c7fe.appspot.com",
-  messagingSenderId: "347259632994",
-  appId: "1:347259632994:web:40736a4d5f35961866803b",
-  measurementId: "G-CC3LV0Z39F"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID ,
+  appId: process.env.REACT_APP_APPID,
+  
 }
 
 const app = initializeApp(firebaseConfig)
